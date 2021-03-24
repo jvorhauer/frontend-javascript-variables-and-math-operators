8d9d7c176e06f87fcac2e06df309737730bf8095
Jurjen Vorhauer <jurjen.vorhauer@jdriven.com>
2021-03-24 16:54:53 +0100

    Voorbereid.

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -4,27 +4,42 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9 wanneer gelogd
 // ==========================================
 
+const nine = 4 + 5;
+console.assert(nine === 9, "nine should be 9, not " + nine);
+console.log("4 + 5 is", nine);
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
 // ==========================================
 
+const b = 7 * 52;
+console.assert(b === 364, "b should be 364, not " + b);
+console.log("7 * 52 is", b);
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
 // ==========================================
 
+const c = (4 + 3) * 7;
+console.assert(c === 49, "c should be 49");
+console.log("4 + 3, times 7 is", c);
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
 // ==========================================
 
+const d = (36 / 6) * 2;
+console.assert(d === 12, "d should be 12");
+console.log("36 divided by 6, mulitplied by 2 is", d);
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
 // ==========================================
 
+const sweet = "zoet";
+const juicy = "sappig";
+console.log("sweet + juicy is", sweet + juicy);
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
@@ -32,9 +47,18 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
 
+const firstName = "Jurjen";
+const lastName = "Vorhauer";
+const fullName = firstName + " " + lastName;
+console.log("full name is " + fullName);
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
 // ==========================================
+
+const locality = "Almere";
+const province = "Flevoland";
+const location = locality + ", " + province;
+console.log("location: " + location);
--- variables.js
+++ variables.js
@@ -3,42 +3,59 @@
 // ==========================================
 
 const start = "Let's go!";
+console.warn("Start! " + start);
 
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const name = "Jurjen";
+console.assert(name === "Jurjen");
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const eyeColor = "brown";
+console.assert(eyeColor === "brown");
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let age = 53;
+console.assert(age === 53, "age must be " + age);
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let occupation = "Developer";
+console.assert(occupation === "Developer", "What? You're a " + occupation + " now? What happened to developer?");
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+const shoeSize = 45;
+console.assert(shoeSize === 45, "Shoe size should be 45");
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let hasPartner = true;
+console.assert(hasPartner, "Bad move, never leave your partner");
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+let childCount = 1;
+console.assert(childCount === 1, "Don't overdo it");
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+
+console.info("Name: " + name + ", eye color: " + eyeColor + ", age: " + age);
